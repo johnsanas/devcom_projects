391c85155be40e6921c76ea418e86dfbc85e2785
John Villete <spinnerjohnv@gmail.com>
2016-12-14 02:38:15 +0800

    Config Files

---
+++ MMU_2016/Generate_Passcode.php
@@ -0,0 +1,13 @@
+<?php
+
+function random_password( $length = 8 ) {
+	 //Simple 
+    $chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    //Complex
+    //$chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-=+;:,.?";
+    $password = substr( str_shuffle( $chars ), 0, $length );
+    return $password;
+}
+
+//echo random_password(4);
