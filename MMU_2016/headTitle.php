3a3e32c8cd38f7be4d9bc4e1876fd41b3711f1b2
Ray Vincent Phillip D. Villaver <phillip60341@gmail.com>
2016-12-15 23:07:08 +0800

    improved head

--- MMU_2016/headTitle.php
+++ MMU_2016/headTitle.php
@@ -7,8 +7,10 @@ $fullname = $row['first_name']." ".$row['last_name'];
 ?>
 
 <div class="headTitle">
-   <a href="logout.php/?logout=logout"><h6><?php echo($fullname); ?></h6></a>
-   <h5>Judge</h5>
+   
+      <a href="logout.php/?logout=logout"><h6><?php echo($fullname); ?></h6></a>
+      <h5>Judge</h5>
+   
    <h1>CATEGORIES</h1>
    <h4>MR & MS UMAK</h4>
    <h3>2016</h3>
